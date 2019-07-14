51d70e914b948626af14dbb22c04e03f4a8c94c4
Victor Garcia <victorjr692@gmail.com>
2019-07-14 10:39:34 -1000

    fix translator

--- site/js/translate_to_marshallese.js
+++ site/js/translate_to_marshallese.js
@@ -3,7 +3,7 @@ function translate_to_marshallese() {
 	var url_parts = window.location.href.split('/');
 	var newUrl = '';
 	
-	if (!url_parts.includes('mh')) {
+	if (url_parts.indexOf('mh') == -1) {
 		url_parts.splice(url_parts.length - 1, 0, 'mh');		
 	}
 	
